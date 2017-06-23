454dea50463996e40044f66029f4342d124db378
Nilpo <devnilpo@gmail.com>
2017-06-23 08:19:44 -0400

    adding secondary option for unicode support--requires PHP with MB support

--- webhook.php
+++ webhook.php
@@ -234,4 +234,10 @@ function clean_string($str) {
 	return $str;
 }
 
+// function unicodeString($str) {
+// 	$str = preg_replace_callback('/\\\\u([0-9a-f]{4})/i', function ($match) {
+// 		return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+// 	}, $str);
+// }
+
 //EOF
